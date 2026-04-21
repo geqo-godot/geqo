225bed7e36c38fd891cc574570cd884ad45ec947
marrero171 <50031007+marrero171@users.noreply.github.com>
2026-04-21 01:49:00 -0400

    Thin C# wrappers around EnvironmentQuery and QueryResult finished

---
+++ project/addons/geqo/csharp_binds/QueryResult2D.cs
@@ -0,0 +1,61 @@
+using Godot;
+using System;
+
+public partial class QueryResult2D
+{
+    private readonly RefCounted result;
+
+    public QueryResult2D(RefCounted refCounted)
+    {
+        result = refCounted;
+    }
+
+    public Godot.Collections.Array<Node2D> GetAllNode()
+    {
+        return (Godot.Collections.Array<Node2D>)result.Call(MethodName.GetAllNode);
+    }
+
+    public Vector2[] GetAllPosition()
+    {
+        return (Vector2[])result.Call(MethodName.GetAllNode);
+
+    }
+
+    //TODO: Implement get_all_results maybe
+
+    public Node2D GetHighestScoreNode()
+    {
+        return (Node2D)result.Call(MethodName.GetHighestScoreNode);
+    }
+
+    public Vector2 GetHighestScorePosition()
+    {
+        return (Vector2)result.Call(MethodName.GetHighestScorePosition);
+    }
+
+    public Node2D GetTopRandomNode(double percent = 0.1)
+    {
+        return (Node2D)result.Call(MethodName.GetTopRandomNode, percent);
+    }
+
+    public Vector2 GetTopRandomPosition(double percent = 0.1)
+    {
+        return (Vector2)result.Call(MethodName.GetTopRandomPosition, percent);
+    }
+
+    public bool HasResult()
+    {
+        return (bool)result.Call(MethodName.HasResult);
+    }
+
+    private static class MethodName
+    {
+        public static readonly StringName GetAllNode = "get_all_node";
+        public static readonly StringName GetAllPosition = "get_all_position";
+        public static readonly StringName GetHighestScoreNode = "get_highest_score_node";
+        public static readonly StringName GetHighestScorePosition = "get_highest_score_position";
+        public static readonly StringName GetTopRandomNode = "get_top_random_node";
+        public static readonly StringName GetTopRandomPosition = "get_top_random_position";
+        public static readonly StringName HasResult = "has_result";
+    }
+}
